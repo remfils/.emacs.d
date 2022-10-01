9c010165fde6bc9af0dcba05326f5bc4efd389da
vlad@tresio.ch <vlad@tresio.ch>
2022-10-01 20:30:51 +0300

    lsp testing

--- init/init-php.el
+++ init/init-php.el
@@ -6,7 +6,6 @@
 ;; TODO: some basic flycheck config
 ;; TODO: configure lsp server support
 
-
 (require-package 'php-mode)
 (require-package 'web-mode)
 (require-package 'emmet-mode)
@@ -58,37 +57,6 @@
 ;; (setq-default company-show-numbers t)
 ;; (setq-default company-idle-delay 0)
 ;; (setq-default company-echo-delay 0)
-
-
-;;;; NOTE: lsp junk
-;; optimisations
-;; https://emacs-lsp.github.io/lsp-mode/page/performance/
-;; (setq gc-cons-threshold 100000000)
-;; (setq read-process-output-max (* 1024 1024))
-;; (add-hook
-;;  'hack-local-variables-hook
-;;  (lambda () (when (derived-mode-p 'php-mode) (lsp) (define-key lsp-mode-map (kbd "C-x C-l") lsp-command-map))))
-
-;; (add-hook 'hack-local-variables-hook (lambda () (when (derived-mode-p 'php-mode) (lsp))))
-
-
-;; (setq lsp-lens-enable nil)
-;; (setq lsp-headerline-breadcrumb-enable nil)
-
-;; (defun remfils-php/toggle-lsp-mode()
-;;   (interactive)
-  
-;;   (if (bound-and-true-p lsp-mode)
-;;       (progn
-;;         (lsp-disconnect)
-;;         (lsp-mode -1))
-;;     (progn
-;;       (setq-local
-;;        lsp-clients-php-server-command (quote ("php" "/home/remfils/Projects/tresio/vendor/felixfbecker/language-server/bin/php-language-server.php")))
-;;       (lsp)
-;;       (define-key lsp-mode-map (kbd "C-x C-l") lsp-command-map))))
- 
-
 
 
 (provide 'init-php)
