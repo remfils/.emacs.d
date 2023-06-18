008e60ffec027ae978af0f9e3882eba744ae7803
Vlad Pereskokov <remfils@ya.ru>
2023-06-18 13:17:35 +0300

    timer

--- init.el
+++ init.el
@@ -15,6 +15,14 @@
 (add-to-list 'load-path (expand-file-name "conf" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "init" user-emacs-directory))
 
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 25))
+            (insert (emacs-init-time))))
+
 ;; (require 'init-benchmarking)
 
 (require 'conf-init)
